8e7d4b74e08d62e41e286ef414aaa018fd19ac2b
Lwq <943904454@qq.com>
2019-04-11 20:03:27 +0800

    完成自定义校验器

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -4,7 +4,9 @@
 # @File    : auth.py
 # @Software: PyCharm
 from wtforms import Form, StringField, PasswordField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+
+from app.models.user import User
 
 
 class RegisterForm(Form):
@@ -14,3 +16,13 @@ class RegisterForm(Form):
     password = PasswordField(validators=[DataRequired(message='密码不能为空，请输入你的密码'), Length(6, 32)])
 
     nickname = StringField(validators=[DataRequired(), Length(2, 10, message='昵称最少2个字符，最多10个字符')])
+
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('电子邮件已被注册')
+
+
+    def validate_nickname(self, field):
+        if User.query.filter_by(nickname=field.data).first():
+            raise ValidationError('昵称已存在')
