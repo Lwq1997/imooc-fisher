b81a5d6803dac9d10891496b4881e9340c3353de
Lwq <943904454@qq.com>
2019-04-12 16:41:56 +0800

    重写filter_by然后重新book_detail的具体逻辑

--- app/models/base.py
+++ app/models/base.py
@@ -6,7 +6,7 @@
 from contextlib import contextmanager
 from datetime import datetime
 
-from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy, BaseQuery
 from sqlalchemy import Column, Integer, SmallInteger
 
 
@@ -21,7 +21,14 @@ class SQLAlchemy(_SQLAlchemy):
             raise e
 
 
-db = SQLAlchemy()
+class Query(BaseQuery):
+    def filter_by(self, **kwargs):
+        if 'status' not in kwargs.keys():
+            kwargs['status'] = 1
+        return super(Query, self).filter_by(**kwargs)
+
+
+db = SQLAlchemy(query_class=Query)
 
 
 class Base(db.Model):
@@ -36,3 +43,10 @@ class Base(db.Model):
         for key, value in attrs.items():
             if hasattr(self, key) and key != 'id':
                 setattr(self, key, value)
+
+    @property
+    def create_datetime(self):
+        if self.create_time:
+            return datetime.fromtimestamp(self.create_time)
+        else:
+            return None
--- app/web/book.py
+++ app/web/book.py
@@ -3,15 +3,18 @@
 # @Author  : Lwq
 # @File    : book.py
 # @Software: PyCharm
-import json
 
-from flask import jsonify, request, Response, render_template, flash
+from flask import request, render_template, flash
+from flask_login import current_user
 
 from app.forms.book import SearchForm
-from app.view_modules.book import BookCollection, BookViewModule
-from app.web.blueprint import web
 from app.libs.helper import is_isbn_or_key
+from app.models.gift import Gift
+from app.models.wish import Wish
 from app.spider.yushu_book import YuShuBook
+from app.view_modules.book import BookCollection, BookViewModule
+from app.view_modules.trade import TradeInfo
+from app.web.blueprint import web
 
 
 @web.route('/book/search')
@@ -42,10 +45,29 @@ def search():
 
 @web.route('/book/<isbn>/detail')
 def book_detail(isbn):
+    has_in_gifts = False
+    has_in_wishes = False
+
+    # 取书籍详情
     yushu_book = YuShuBook()
     yushu_book.search_by_isbn(isbn)
     book = BookViewModule(yushu_book.first)
-    return render_template('book_detail.html', book=book)
+
+    if current_user.is_authenticated:
+        if Gift.query.filter_by(uid=current_user.id, launched=False, isbn=isbn).first:
+            has_in_gifts = True
+        if Wish.query.filter_by(uid=current_user.id, launched=False, isbn=isbn).first:
+            has_in_wishes = True
+
+    trade_gifts = Gift.query.filter_by(isbn=isbn, launched=False).all()
+    trade_wishes = Wish.query.filter_by(isbn=isbn, launched=False).all()
+
+    trade_gifts_model = TradeInfo(trade_gifts)
+    trade_wishes_model = TradeInfo(trade_wishes)
+
+    return render_template('book_detail.html', book=book,
+                           has_in_gifts=has_in_gifts, has_in_wishes=has_in_wishes,
+                           wishes=trade_wishes_model, gifts=trade_gifts_model)
 
 
 @web.route('/test/local')
